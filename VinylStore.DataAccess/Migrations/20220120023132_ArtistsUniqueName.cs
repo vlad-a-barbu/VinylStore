13537a1a57c5ffb1a89f9c75e5e83126555e8ada
Vlad Alexandru Barbu <vlad.a.brb@gmail.com>
2022-01-20 04:54:52 +0200

    artists unique name constraint migration

---
+++ VinylStore.DataAccess/Migrations/20220120023132_ArtistsUniqueName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VinylStore.DataAccess.Migrations
+{
+    public partial class ArtistsUniqueName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Artists",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_Name",
+                table: "Artists",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Artists_Name",
+                table: "Artists");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Artists",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
